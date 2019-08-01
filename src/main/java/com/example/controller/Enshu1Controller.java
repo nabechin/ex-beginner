57ab18e407087401a2f0ab406e0b9ee0de389411
nabenabe0258 <nabenabe0258@gmail.com>
2019-08-01 16:38:55 +0900

    コントローラーを作成しました

--- src/main/java/com/example/controller/Enshu1Controller.java
+++ src/main/java/com/example/controller/Enshu1Controller.java
@@ -1,9 +1,11 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Member;
 import com.example.form.Enshu1Form;
 
 @Controller
@@ -18,6 +20,14 @@ public class Enshu1Controller {
 	@RequestMapping("")
 	public String index() {
 		return "exam01";
+	}
+	
+	@RequestMapping("/receive-form")
+	public String receiveForm(Model model,Enshu1Form enshu1Form) {
+		Member member = new Member();
+		member.setName(enshu1Form.getName());
+		model.addAttribute("member",member);
+		return "exam01-result";
 	}
 	
 }
